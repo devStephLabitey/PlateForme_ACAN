66d18bdffe0598d48b200ac1a1bb111e39720d56
stephdev <labiteysteph@gmail.com>
2024-06-15 19:16:18 +0000

    Last Commit

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,29 @@ burger.addEventListener('click',()=>{
   console.log(1);
 })
 console.log(burger, navlink);
+
+
+// typing text animation
+
+const texts = ["Académie", "Numérique", "ACAN"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById('text').textContent = letter;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+        setTimeout(type, 1000); // Delay before starting the next word
+    } else {
+        setTimeout(type, 200); // Typing speed
+    }
+}());
